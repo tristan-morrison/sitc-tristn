e7303cf6478d681d9dd578016ace5776753b2e31
Tristan Lopus <tmlopus96@gmail.com>
2019-01-22 00:26:30 -0800

    Further development; Finished Airtable requests and worked on UI

--- src/components/VolunteerList.js
+++ src/components/VolunteerList.js
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom';
 import Airtable from 'airtable';
 import * as loglevel from 'loglevel';
 
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-import ListItemAvatar from '@material-ui/core/ListItemAvatar'
-import Avatar from '@material-ui/core/Avatar'
-import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import Avatar from '@material-ui/core/Avatar';
+import Badge from '@material-ui/core/Badge';
+import Typography from '@material-ui/core/Typography';
 import SvgIcon from '@material-ui/core/SvgIcon';
+import DriveEtaIcon from '@material-ui/icons/DriveEta';
+import AirlineSeatReclineNormalIcon from '@material-ui/icons/AirlineSeatReclineNormal';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+
+const styles = theme => ({
+  avatar: {
+    backgroundColor: theme.palette.primary[500]
+  }
+});
 
 class VolunteerList extends React.Component {
   constructor (props) {
@@ -20,7 +34,7 @@ class VolunteerList extends React.Component {
   }
 
   // gets volunteer info from the Profiles table
-  componentWillMount () {
+  componentDidMount () {
     let self = this;
 
     const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.VOLUNTEERS_BASE_ID);
@@ -52,29 +66,52 @@ class VolunteerList extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
     const teerListItems = [];
     this.state.volunteerInfo.forEach((record) => {
-      teerListItems.push(
-        <ListItem key = {record.get('PersonID')}>
-          <ListItemAvatar>
-            <SvgIcon>
-              <rect x="4.8" y="14.6" transform="matrix(0.6211 -0.7837 0.7837 0.6211 -8.8542 13.1528)" width="8.7" height="2.2"/>
-              <path d="M12,2C6.5,2,2,6.5,2,12s4.5,10,10,10c5.5,0,10-4.5,10-10S17.5,2,12,2z M12,20c-4.4,0-8-3.6-8-8s3.6-8,8-8s8,3.6,8,8
-              S16.4,20,12,20z"/>
-              <path style={{fill:'none'}} d="M0,0h24v24H0V0z"/>
-              <rect x="3.2" y="11.2" width="18" height="2.2"/>
-              <rect x="14" y="11.4" transform="matrix(0.7837 -0.6211 0.6211 0.7837 -6.4938 12.8169)" width="2.2" height="8.7"/>
-              <ellipse cx="12" cy="12" rx="4.2" ry="4.2"/>
-              <path style={{fill:'#FFFFFF'}} d="M12,8.4c1.9,0,3.5,1.6,3.5,3.6s-1.6,3.6-3.5,3.6S8.5,14,8.5,12S10.1,8.4,12,8.4 M12,6.7
-              c-2.9,0-5.2,2.4-5.2,5.3s2.3,5.3,5.2,5.3s5.2-2.4,5.2-5.3S14.9,6.7,12,6.7L12,6.7z"/>
 
-            </SvgIcon>
-          </ListItemAvatar>
-          <ListItemText
-            primary = {record.get('First Name')}>
-          </ListItemText>
-        </ListItem>
-      );
+      // set the icon to go in this person's avatar
+      let avatarIcon;
+      if (record.get('Has Car')) {
+        avatarIcon = (
+          <DriveEtaIcon />
+        );
+      } else {
+        avatarIcon = (
+          <AirlineSeatReclineNormalIcon />
+        );
+      }
+
+      // set up the avatar with or without a badge
+      let avatar = '';
+      if (record.get('Paid')) {
+        avatar = avatarIcon;
+      } else {
+        avatar = (
+          <Badge badgeContent="$" color="secondary">
+            {avatarIcon}
+          </Badge>
+        );
+      }
+
+      return (
+        teerListItems.push(
+          <ListItem key = {record.get('PersonID')}>
+            <ListItemIcon>
+              {avatar}
+            </ListItemIcon>
+            <ListItemText
+              primary = {record.get('First Name') + ' ' + record.get('Last Name')}
+              secondary = {record.get('Hours Credited') + ' Hours'}>
+            </ListItemText>
+            <ListItemSecondaryAction>
+              <IconButton aria-label="Check In">
+                <ArrowForwardIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        )
+      )
     })
 
     return (
@@ -87,4 +124,4 @@ class VolunteerList extends React.Component {
 
 }
 
-export default VolunteerList;
+export default withStyles(styles)(VolunteerList);
