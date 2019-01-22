1bd3c4242bbd74dc8e372c19da7d21fcc89d0d14
Tristan Lopus <tmlopus96@gmail.com>
2019-01-22 00:26:53 -0800

    Added theme

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import * as log from 'loglevel'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import lightBlue from '@material-ui/core/colors/lightBlue';
+import deepOrange from '@material-ui/core/colors/deepOrange';
 
 import App from './components/App';
 
 log.setLevel("trace");
 
 const theme = createMuiTheme({
+  palette: {
+    primary: lightBlue,
+    secondary: deepOrange
+  },
   typography: {
     useNextVariants: true,
   },
