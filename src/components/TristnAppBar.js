1fe21d45fc04e3e189dfc4c9f945b94426ad9037
Tristan Lopus <tmlopus96@gmail.com>
2019-01-24 00:51:20 -0800

    Dynamically-sized search bar

--- src/components/TristnAppBar.js
+++ src/components/TristnAppBar.js
@@ -11,6 +11,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import InputBase from '@material-ui/core/InputBase';
+import Input from '@material-ui/core/Input';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 
@@ -34,6 +35,7 @@ const styles = theme => ({
   },
   searchContainer: {
     position: 'relative',
+    transition: theme.transitions.create('width'),
     minWidth: theme.spacing.unit * 4,
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
@@ -43,12 +45,15 @@ const styles = theme => ({
     marginLeft: 0,
     width: 'auto',
     [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing.unit,
+      // marginLeft: theme.spacing.unit,
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: 32
     }
   },
   inputRoot: {
     color: 'inherit',
-    width: '100%'
+    width: '100%',
   },
   inputInput: {
     transition: theme.transitions.create('width'),
@@ -57,30 +62,81 @@ const styles = theme => ({
       paddingTop: theme.spacing.unit,
       paddingRight: theme.spacing.unit,
       paddingBottom: theme.spacing.unit,
-      paddingLeft: theme.spacing.unit* 10,
-      width: 120,
+      paddingLeft: theme.spacing.unit* 5,
+      // width: 120,
       '&:focus': {
-        width: 200
+        // width: 200
       }
     },
     [theme.breakpoints.down('sm')]: {
-      width: 5,
+      paddingTop: theme.spacing.unit,
+      paddingRight: theme.spacing.unit,
+      paddingBottom: theme.spacing.unit,
+      paddingLeft: theme.spacing.unit* 5,
+      width: '100%',
       '&:focus': {
-        paddingTop: theme.spacing.unit,
-        paddingRight: theme.spacing.unit,
-        paddingBottom: theme.spacing.unit,
-        paddingLeft: theme.spacing.unit* 5,
-        width: 500
+        // width: 500
       }
+    },
+    '&$underline:before': {
+      borderBottom: 0
     }
   }
 });
 
 class TristnAppBar extends React.Component {
 
+  constructor () {
+    super();
+
+    this.state = {
+      searchText: '',
+      searchIsFocused: false,
+    }
+
+    this.updateSearchText = this.updateSearchText.bind(this);
+    this.onSearchFocused = this.onSearchFocused.bind(this);
+    this.onSearchBlur = this.onSearchBlur.bind(this);
+  }
+
+  updateSearchText (event) {
+    loglevel.debug(event.target.value);
+    this.setState({searchText: event.target.value});
+  }
+
+  onSearchBlur () {
+    this.setState({searchIsFocused: !this.state.searchIsFocused});
+  }
+
+  onSearchFocused () {
+    this.setState({searchIsFocused: !this.state.searchIsFocused});
+  }
+
+
   render() {
     const { classes } = this.props;
     loglevel.info(this.props.theme.breakpoints.up('sm'));
+
+    let searchContainerStyle = {};
+    let searchInputStyle = {};
+    if (this.state.searchIsFocused) {
+      searchContainerStyle = {
+        width: 500
+      }
+    }
+    else {
+      if (this.state.searchText.length > 0) {
+        searchContainerStyle = {
+          width: (this.state.searchText.length * 8) + 50
+        }
+      } else {
+        searchInputStyle = {
+          padding: 0
+        }
+      }
+    }
+
+
 
     return (
       <AppBar position="static">
@@ -92,15 +148,21 @@ class TristnAppBar extends React.Component {
             Tristn
           </Typography>
           <div className={classes.growSpacer}></div>
-          <div className={classes.searchContainer}>
+          <div className={classes.searchContainer} style={searchContainerStyle}>
             <div className={classes.searchIcon}>
               <SearchIcon />
             </div>
-            <InputBase
+            <Input
+              disableUnderline = "true"
+              value = {this.state.searchText}
+              onChange = {this.updateSearchText}
+              onFocus = {this.onSearchFocused}
+              onBlur = {this.onSearchBlur}
               classes={{
                 root: classes.inputRoot,
                 input: classes.inputInput
               }}
+              style={searchInputStyle}
             />
           </div>
         </Toolbar>
