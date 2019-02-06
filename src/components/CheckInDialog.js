240666c535a70dfd7d72aa33d62f79de72f005c4
Tristan Lopus <tristan.lopus@gmail.com>
2019-02-06 09:33:20 -0800

    Removed Transition component

--- src/components/CheckInDialog.js
+++ src/components/CheckInDialog.js
@@ -10,7 +10,6 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogActions from '@material-ui/core/DialogActions';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Collapse from '@material-ui/core/Collapse';
 
 const styles = theme => ({
   dialogContent: {
@@ -49,40 +48,7 @@ class CheckInDialog extends React.Component {
 
     return (
       <Dialog onClose={this.close} open={this.state.open}>
-        <Transition in={false} timeout={1000}>
-          <div className={classes['exited']}>
-            <DialogContent className={classes.dialogContent}>
-              <DialogContentText>
-                Did {firstName} pay the registration fee today?
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button>
-                No
-              </Button>
-              <Button color="primary">
-                Yes
-              </Button>
-            </DialogActions>
-          </div>
-        </Transition>
-        <Transition in={this.state.showDriver} timeout={1000}>
-          <div>
-            <DialogContent className={classes.dialogContent}>
-              <DialogContentText>
-                Can {firstName} drive today?
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button>
-                No
-              </Button>
-              <Button color="primary">
-                Yes
-              </Button>
-            </DialogActions>
-          </div>
-        </Transition>
+        
       </Dialog>
     );
   }
