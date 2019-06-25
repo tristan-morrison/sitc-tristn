ea6a3272f9ec360e50fff36d89c64ce210981952
sitc_tristn <sitc_tristn@ps595770.dreamhostps.com>
2019-06-25 02:01:35 -0700

    fix

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 import path from 'path';
 import session from 'express-session';
-import sessionFileStore from 'session-file-store'
+import sessionFileStore from 'session-file-store';
 import passport from 'passport';
 import googleOAuth from 'passport-google-oauth';
 import dotenv from 'dotenv';
@@ -30,16 +32,14 @@ const app = express();
 const GoogleStrategy = googleOAuth.OAuth2Strategy;
 const FileStore = sessionFileStore(session);
 
-app.use(express.static(path.join(__dirname, './../../')));
-
 passport.use(new GoogleStrategy({
     clientID: OAUTH.CLIENT_ID,
     clientSecret: process.env.GOOGLE_OAUTH_SECRET,
-    callbackURL: "http://tristn.summerinthecity.com:3030/siteSelect"
+    callbackURL: process.env.DOMAIN,
   },
   function(accessToken, refreshToken, profile, done) {
     logger.info("profile.id: " + util.inspect(profile));
-    return done(null, { username: 'tristan@summerinthecity.com', password: 'Tko-14-aaR', id: profile.id }); //, {message: 'Invalid credentials\n'}
+    return done(null, { username: 'tristan@summerinthecity.com', password: 'dfkjsldjl', id: profile.id }); //, {message: 'Invalid credentials\n'}
     // User.findOrCreate({ googleId: profile.id }, function (err, user) {
     //   return done(err, user);
     // });
@@ -51,36 +51,65 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  done(null, id);
+  done(null, { username: 'tristan@summerinthecity.com', password: 'dfkjsldjl', id: id });
 })
 
+const cookieExpirationDate = new Date();
+const cookieExpirationDays = 3;
+cookieExpirationDate.setDate(cookieExpirationDate.getDate() + cookieExpirationDays);
+
+app.use(cookieParser(process.env.SESSION_SECRET));
+app.use(bodyParser());
 app.use(session({
-  genid: (req) => {
-    return uuid();
-  },
+  // genid: (req) => {
+  //   return uuid();
+  // },
   store: new FileStore(),
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUnintialized: true,
+  saveUninitialized: true,
+  cookie: {
+    expires: cookieExpirationDate
+  }
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/auth', passport.authenticate('google', {
-  scope: ['openid', 'https://www.googleapis.com/auth/userinfo.email']}));
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    // req.user is available for use here
+    logger.info(util.inspect(req))
+    return next();
+  }
+
+  // denied. redirect to login
+  res.redirect("/auth");
+}
+
+app.get('/auth/success', passport.authenticate('google', { scope: ['openid'], failureRedirect: '/authFail' }), function (req, res) {
+  logger.info(util.inspect(req.session));
+  res.redirect("/siteSelect");
+})
 
 app.get('/authFail', (req, res) => {
   res.send("Authentication failed.");
 })
 
-app.get('/siteSelect', passport.authenticate('google', { scope: ['openid', 'https://www.googleapis.com/auth/userinfo.email'], failureRedirect: '/authFail' }), function (req, res) {
-  logger.info(util.inspect(req));
-  res.redirect('/');
-})
+app.get('/auth', passport.authenticate('google', { scope: ['openid'] }));
 
-app.get('*', (req, res) => {
+app.get('/siteSelect', ensureAuthenticated, (req, res) => {
   res.sendFile(path.join(__dirname, '../../index.html'));
 })
+
+app.get('/aProtectedRoute', ensureAuthenticated, (req, res) => {
+  res.send("YOU ARE AUTHENTICATED. CONGRATS.")
+})
+
+app.get('/', ensureAuthenticated, (req, res) => {
+  res.sendFile(path.join(__dirname, '../../index.html'));
+})
+
+app.use(express.static(path.join(__dirname, './../../')));
 
 app.listen(3030, () => {
   console.log('Listening on localhost:3030')
